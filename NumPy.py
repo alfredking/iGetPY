d70b87060144641171cc1b617b1321e358fb07b9
alfredking <alfredkingwhut@gmail.com>
2019-01-09 20:47:37 +0800

    numpy2

--- NumPy.py
+++ NumPy.py
@@ -37,3 +37,57 @@ print(np.multiply(x1, x2))
 print(np.divide(x1, x2))
 print(np.power(x1, x2))
 print(np.remainder(x1, x2))
+print(np.mod(x1,x2))
+
+
+#统计函数
+
+#计数组 / 矩阵中的最大值函数 amax()最小值函数 amin
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(np.amin(a))
+print(np.amin(a[0]))
+print(np.amin(a[1]))
+print(np.amin(a, 0))
+print(np.amin(a, 1))
+print(np.amax(a))
+print(np.amax(a[0]))
+print(np.amax(a[1]))
+print(np.amax(a, 0))
+print(np.amax(a, 1))
+
+#统计最大值与最小值之差 ptp()
+print(np.ptp(a))
+print(np.ptp(a, 0))
+print(np.ptp(a, 1))
+
+#统计数组的百分位数 percentile()
+print(np.percentile(a, 50))
+print(np.percentile(a, 50, axis=0))
+print(np.percentile(a, 40, axis=1))
+
+#统计数组中的中位数 median()、平均数 mean()
+print(np.median(a))
+print(np.median(a, 0))
+print(np.median(a, 1))
+
+print(np.mean(a))
+print(np.mean(a, 0))
+print(np.mean(a, 1))
+
+#统计数组中的加权平均值 average()
+a = np.array([1, 2, 3, 4])
+wts = np.array([1, 2, 3, 4])
+print(np.average(a))
+print(np.average(a, weights=wts))
+
+#统计数组中的标准差 std()、方差 var()
+print(np.std(a))
+print(np.var(a))
+
+#NumPy 排序
+a = np.array([[4, 3, 2], [2, 4, 1]])
+print(np.sort(a))
+print(np.sort(a, axis=None))
+print(np.sort(a, axis=0))
+print(np.sort(a,axis=1))
+print(np.sort(a, axis=-1, kind='quicksort', order=None))
