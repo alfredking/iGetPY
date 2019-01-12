1a4950ee1c0cc7d7b6b0d2dd6d09f15b10d9c956
alfredking <alfredkingwhut@gmail.com>
2019-01-12 10:41:07 +0800

    df1 df2

--- Pandas.py
+++ Pandas.py
@@ -6,13 +6,14 @@ x2 = Series(data=[1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
 print(x1)
 print(x2)
 
-d = {'a':1, 'b':2, 'c':3, 'd':4}
+d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 x3 = Series(d)
 print(x3)
 
 data = {'Chinese': [66, 95, 93, 90, 80], 'Englis': [65, 85, 92, 88, 90], 'Math': [30, 98, 96, 77, 90]}
 df1 = DataFrame(data)
-df2 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'ZhaoYun', 'HuangZhong', 'DianWei'], colums = ['Englis', 'Math', 'Chinese'])
+df2 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'ZhaoYun', 'HuangZhong', 'DianWei'], columns=['Englis', 'Math', 'Chinese'])
 print(df1)
 print(df2)
+
 
