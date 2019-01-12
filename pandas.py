eb21705bb0eb6a200beb5c80eb1aeb5ffa2ec50d
alfredking <alfredking@126.com>
2019-01-12 10:30:39 +0800

    pandas

--- pandas.py
+++ pandas.py
@@ -10,4 +10,9 @@ d = {'a':1, 'b':2, 'c':3, 'd':4}
 x3 = Series(d)
 print(x3)
 
-data = {'Chinese': [66, 95, 93, 90, 80], 'Englis': [65, 85, 92, 88, 90], }
+data = {'Chinese': [66, 95, 93, 90, 80], 'Englis': [65, 85, 92, 88, 90], 'Math': [30, 98, 96, 77, 90]}
+df1 = DataFrame(data)
+df2 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'ZhaoYun', 'HuangZhong', 'DianWei'], colums = ['Englis', 'Math', 'Chinese'])
+print(df1)
+print(df2)
+
