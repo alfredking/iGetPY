ba61df8d0aed324b9419ce93edbe30c4c6b47b2f
alfredking <alfredking@126.com>
2019-01-13 23:56:49 +0800

    pandas apply

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pandas import Series, DataFrame
 
 x1 = Series([1, 2, 3, 4])
@@ -16,11 +17,62 @@ df2 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'ZhaoYun', 'HuangZhong', 'Dia
 print(df1)
 print(df2)
 
-df2.to_excel('data.xlsx')
+#df2.to_excel('data.xlsx')
+#score = DataFrame(pd.read_excel('data.xlsx'))
+#score.to_excel('data1.xlsx')
+#print(score)
+
+df2 = df2.drop(columns=['Chinese'])
+df2 = df2.drop(index=['ZhangFei'])
+df2.rename(columns={'chinese': 'YuWen', 'Englis': 'Yingyu'}, inplace=True)
+df2 = df2.drop_duplicates()
+df2['Math'].astype(np.int64)
+df2['Math'] = df2['Math'].astype('str')
+print(df2['Math'])
+
+# 删除左右两边空格
+df2['Math'] = df2['Math'].map(str.strip)
+# 删除左边空格
+df2['Math'] = df2['Math'].map(str.lstrip)
+# 删除右边空格
+df2['Math'] = df2['Math'].map(str.rstrip)
+
+df2['Math'] = df2['Math'].str.strip('$')
+
+
+# 全部大写
+df2.columns = df2.columns.str.upper()
+# 全部小写
+df2.columns = df2.columns.str.lower()
+# 首字母大写
+df2.columns = df2.columns.str.title()
+
+print('case transform')
+
 score = DataFrame(pd.read_excel('data.xlsx'))
-score.to_excel('data1.xlsx')
+print(score.isnull())
+print(score.isnull().any())
+
+# apply函数
+score['姓名'] = score['姓名'].apply(str.upper)
+
+
+def double_df(x):
+    return 2*x
+
+
+score[u'语文'] = score[u'语文'].apply(double_df)
+
 print(score)
 
 
+def plus(df, n, m):
+    df['new1'] = (df[u'语文']+df[u'英语'])*m
+    df['new2'] = (df[u'语文']+df[u'英语'])*n
+    return df
 
 
+print(score)
+# score = score.apply(plus, axis=1, args=(2, 3))
+score = score.apply(plus, axis=0, args=(2, 3))
+print(score)
