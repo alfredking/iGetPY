5e303764da4b17f5dd6b7c59cabab58e2c2d1370
jinqibing <jinqibing@cmhi.chinamobile.com>
2019-04-21 18:18:38 +0800

    pagerank review

--- email_pr.py
+++ email_pr.py
@@ -6,18 +6,60 @@ import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
+
+# 画网络图
+def show_graph(graph):
+    # 使用Spring Layout布局，类似中心放射状
+    positions = nx.spring_layout(graph)
+    # 设置网络图中的节点大小，大小与pagerank值相关，因为pagerank值很小所以需要*20000
+    for v, x in graph.nodes(data=True):
+        print(v, x)
+    nodesize = [x['pagerank']*20000 for v, x in graph.nodes(data=True)]
+    # 设置网络图中的边长度
+    for e in graph.edges(data=True):
+        print(e)
+    edgesize = [np.sqrt(e[2]['weight']) for e in graph.edges(data=True)]
+    # 绘制节点
+    nx.draw_networkx_nodes(graph, positions, node_size=nodesize, alpha=0.4)
+    # 绘制边
+    nx.draw_networkx_edges(graph, positions, edge_size=edgesize, alpha=0.2)
+    # 绘制节点的label
+    nx.draw_networkx_labels(graph, positions, font_size=10)
+    # 输出希拉里邮件中的所有人物关系图
+    plt.show()
+
+
+# 创建有向图
+G = nx.DiGraph()
+# 有向图之间边的关系
+edges = [("A", "B"), ("A", "C"), ("A", "D"), ("B", "A"), ("B", "D"), ("C", "A"), ("D", "B"), ("D", "C")]
+weight_edges = defaultdict(list)
+
+weight_edges = [(edge[0], edge[1], 1) for edge in edges]
+G.add_weighted_edges_from(weight_edges)
+
+# pagerank_list = nx.pagerank(G, alpha=1)
+pagerank = nx.pagerank(G)  # alpha默认是0.85,代表用户有 15% 的概率随机跳转,85%的可能性是通过链接浏览网页
+print("pagerank 值是：", pagerank)
+pagerank_list = {node: rank for node, rank in pagerank.items()}
+# 将pagerank数值作为节点的属性
+nx.set_node_attributes(G, name='pagerank', values=pagerank_list)
+show_graph(G)
+
+
+'''
 # 数据加载
 emails = pd.read_csv("./input/Emails.csv")
 # 读取别名文件
 file = pd.read_csv("./input/Aliases.csv")
 aliases = {}
 for index, row in file.iterrows():
-    aliases[row['Alias']] = row['PersonId']
+    aliases[row['Alias']] = row['PersonId']  # 通过这一步操作可以通过别名找到PersonId
 # 读取人名文件
 file = pd.read_csv("./input/Persons.csv")
 persons = {}
 for index, row in file.iterrows():
-    persons[row['Id']] = row['Name']
+    persons[row['Id']] = row['Name']  # 通过这一步操作可以通过PersonId找到真名
 
 
 # 针对别名进行转换        
@@ -30,24 +72,6 @@ def unify_name(name):
     if name in aliases.keys():
         return persons[aliases[name]]
     return name
-
-
-# 画网络图
-def show_graph(graph):
-    # 使用Spring Layout布局，类似中心放射状
-    positions = nx.spring_layout(graph)
-    # 设置网络图中的节点大小，大小与pagerank值相关，因为pagerank值很小所以需要*20000
-    nodesize = [x['pagerank']*20000 for v, x in graph.nodes(data=True)]
-    # 设置网络图中的边长度
-    edgesize = [np.sqrt(e[2]['weight']) for e in graph.edges(data=True)]
-    # 绘制节点
-    nx.draw_networkx_nodes(graph, positions, node_size=nodesize, alpha=0.4)
-    # 绘制边
-    nx.draw_networkx_edges(graph, positions, edge_size=edgesize, alpha=0.2)
-    # 绘制节点的label
-    nx.draw_networkx_labels(graph, positions, font_size=10)
-    # 输出希拉里邮件中的所有人物关系图
-    plt.show()
 
 
 # 将寄件人和收件人的姓名进行规范化
@@ -90,3 +114,5 @@ for n, p_rank in graph.nodes(data=True):
         small_graph.remove_node(n)
 # 画网络图
 show_graph(small_graph)
+'''
+
