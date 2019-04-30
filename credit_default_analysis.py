488eb06fd79455305d308678420cfc6e89900e72
jinqibing <jinqibing@cmhi.chinamobile.com>
2019-04-30 20:15:56 +0800

    credit card and bitcoin

--- credit_default_analysis.py
+++ credit_default_analysis.py
@@ -22,10 +22,10 @@ next_month = data['default.payment.next.month'].value_counts()
 print(next_month)
 df = pd.DataFrame({'default.payment.next.month': next_month.index,'values': next_month.values})
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.figure(figsize = (6,6))
+plt.figure(figsize=(6, 6))
 plt.title('信用卡违约率客户\n (违约：1，守约：0)')
 sns.set_color_codes("pastel")
-sns.barplot(x = 'default.payment.next.month', y="values", data=df)
+sns.barplot(x='default.payment.next.month', y="values", data=df)
 locs, labels = plt.xticks()
 plt.show()
 # 特征选择，去掉ID字段、最后一个结果字段即可
@@ -39,10 +39,10 @@ train_x, test_x, train_y, test_y = train_test_split(features, target, test_size=
     
 # 构造各种分类器
 classifiers = [
-    SVC(random_state = 1, kernel = 'rbf'),    
-    DecisionTreeClassifier(random_state = 1, criterion = 'gini'),
-    RandomForestClassifier(random_state = 1, criterion = 'gini'),
-    KNeighborsClassifier(metric = 'minkowski'),
+    SVC(random_state=1, kernel='rbf'),
+    DecisionTreeClassifier(random_state=1, criterion='gini'),
+    RandomForestClassifier(random_state=1, criterion='gini'),
+    KNeighborsClassifier(metric='minkowski'),
 ]
 # 分类器名称
 classifier_names = [
@@ -58,11 +58,12 @@ classifier_param_grid = [
             {'randomforestclassifier__n_estimators':[3,5,6]} ,
             {'kneighborsclassifier__n_neighbors':[4,6,8]},
 ]
- 
+
+
 # 对具体的分类器进行GridSearchCV参数调优
-def GridSearchCV_work(pipeline, train_x, train_y, test_x, test_y, param_grid, score = 'accuracy'):
+def GridSearchCV_work(pipeline, train_x, train_y, test_x, test_y, param_grid, score='accuracy'):
     response = {}
-    gridsearch = GridSearchCV(estimator = pipeline, param_grid = param_grid, scoring = score)
+    gridsearch = GridSearchCV(estimator = pipeline, param_grid = param_grid, scoring=score)
     # 寻找最优的参数 和最优的准确率分数
     search = gridsearch.fit(train_x, train_y)
     print("GridSearch最优参数：", search.best_params_)
@@ -72,10 +73,11 @@ def GridSearchCV_work(pipeline, train_x, train_y, test_x, test_y, param_grid, sc
     response['predict_y'] = predict_y
     response['accuracy_score'] = accuracy_score(test_y,predict_y)
     return response
- 
+
+
 for model, model_name, model_param_grid in zip(classifiers, classifier_names, classifier_param_grid):
     pipeline = Pipeline([
             ('scaler', StandardScaler()),
             (model_name, model)
     ])
-    result = GridSearchCV_work(pipeline, train_x, train_y, test_x, test_y, model_param_grid , score = 'accuracy')
+    result = GridSearchCV_work(pipeline, train_x, train_y, test_x, test_y, model_param_grid, score='accuracy')
